dc05d5e00dff0df48c58476451b58dae557df737
TeddyGwyn <TeddyGwyn>
2026-06-25 17:44:23 +0200

    armory patch 5

--- pwm-project/frontend/src/app/game/match/match.page.ts
+++ pwm-project/frontend/src/app/game/match/match.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, OnDestroy, ChangeDetectorRef, ViewChild, NgZone, HostListener } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ChangeDetectorRef, ViewChild, NgZone, HostListener, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ModalController, MenuController, ToastController, ActionSheetController } from '@ionic/angular'; // <--- AGGIUNTO MenuController
